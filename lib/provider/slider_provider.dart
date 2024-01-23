bc6f40cdd248498518c188a1c8c52c53e25f77a2
aakashshivgan <aakashshivgan7739@gmail>
2024-01-23 22:24:41 +0530

    mdodig slider app

--- lib/provider/slider_provider.dart
+++ lib/provider/slider_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SliderProvider with ChangeNotifier {
-  double _value = 0;
+  double _value = 0.5;
   double get value => _value;
 
   void setValue(double val) {
--- lib/screen/slider_example.dart
+++ lib/screen/slider_example.dart
@@ -15,40 +15,42 @@ class _SliderExampleState extends State<SliderExample> {
     print('build');
     final sliderProvider = Provider.of<SliderProvider>(context, listen: false);
     return Scaffold(
-      body: Column(
-        children: [
-          Consumer<SliderProvider>(
-            builder: (context, value, child) {
-              return Slider(
-                  min: 0,
-                  max: 1,
-                  value: value.value,
-                  onChanged: (val) {
-                    sliderProvider.setValue(val);
-                  });
-            },
-          ),
-          Consumer<SliderProvider>(
-            builder: (context, value, child) {
-              return Row(
-                children: [
-                  Expanded(
-                      child: Container(
-                    height: 100,
-                    decoration: BoxDecoration(
-                        color: Colors.red.withOpacity(value.value)),
-                  )),
-                  Expanded(
-                      child: Container(
-                    height: 100,
-                    decoration: BoxDecoration(
-                        color: Colors.green.withOpacity(value.value)),
-                  ))
-                ],
-              );
-            },
-          )
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Consumer<SliderProvider>(
+              builder: (context, value, child) {
+                return Slider(
+                    min: 0,
+                    max: 1,
+                    value: value.value,
+                    onChanged: (val) {
+                      sliderProvider.setValue(val);
+                    });
+              },
+            ),
+            Consumer<SliderProvider>(
+              builder: (context, value, child) {
+                return Row(
+                  children: [
+                    Expanded(
+                        child: Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                          color: Colors.red.withOpacity(value.value)),
+                    )),
+                    Expanded(
+                        child: Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                          color: Colors.green.withOpacity(value.value)),
+                    ))
+                  ],
+                );
+              },
+            )
+          ],
+        ),
       ),
     );
   }
